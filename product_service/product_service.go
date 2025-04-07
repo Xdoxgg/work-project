3279d7cb1955eb93a210b4bd1b9143fb7036ad3c
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-04-07 20:49:37 +0300

    internal commit

---
+++ product_service/product_service.go
@@ -0,0 +1,15 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Product Service: Hello, Product!")
+}
+
+func main() {
+	http.HandleFunc("/products", productHandler)
+	http.ListenAndServe(":8082", nil)
+}
---
+++ user_service/user_service.go
@@ -0,0 +1,16 @@
+﻿// user_service.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "User Service: Hello, User!")
+}
+
+func main() {
+	http.HandleFunc("/users", userHandler)
+	http.ListenAndServe(":8081", nil)
+}
