493a450be12b84163a8492e9e531e2a3f180c189
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-04-08 18:17:16 +0300

    fix dockerfile for users service

--- user_service/go.mod
+++ user_service/go.mod
@@ -1,5 +1,5 @@
 module user_service
 
-go 1.23
+go 1.21
 
 require github.com/lib/pq v1.10.9 // indirect
--- user_service/user_service.go
+++ user_service/user_service.go
@@ -1,7 +1,9 @@
-﻿// user_service.go
-package main
+﻿package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"net/http"
 )
 
@@ -9,6 +11,21 @@ type User struct {
 	ID       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func connectDB() (*sql.DB, error) {
+	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Ошибка подключения к базе данных:", err)
+		return nil, err
+	}
+	// Проверяем соединение
+	if err = db.Ping(); err != nil {
+		fmt.Println("Не удалось подключиться к базе данных:", err)
+		return nil, err
+	}
+	return db, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +40,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getUsersHandler(w http.ResponseWriter, r *http.Request) {
-	
+	db, err := connectDB()
+	rows, err := db.Query("SELECT id, email, password FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Email, &user.Password); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
 }
 
 func postUsersHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,6 +64,11 @@ func postUsersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	_, err := connectDB()
+	if err != nil {
+		fmt.Println("conneting to db faled")
+		return
+	}
 	http.HandleFunc("/api/users", handler)
 	http.ListenAndServe(":8082", nil)
 }
