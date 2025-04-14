f7dd715c716f5ff4c9b55ff4f1a0e8b7f5a345b2
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-04-14 19:14:34 +0300

    Update schema.sql

--- app/schema.sql
+++ app/schema.sql
@@ -14,3 +14,21 @@ CREATE TABLE IF NOT EXISTS movies(
     description VARCHAR(255) NOT NULL,
     year DATE
 );
+
+
+CREATE TABLE IF NOT EXISTS genres(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tags(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+
+Table movies_to_tags{
+  id serial [primary key]
+  tag_id INTEGER 
+  movie_id INTEGER
+}
